2ab74ce671661a7cc270dbedb3dc28d45298747e
boris <borisov21011986@rambler.ru>
2021-08-14 22:45:22 +0400

    Подключил Retrofit.Изменил ресурсы и поменял меню и макеты

--- app/src/main/java/com/android/filmlibrary/Constant.kt
+++ app/src/main/java/com/android/filmlibrary/Constant.kt
@@ -3,13 +3,12 @@ package com.android.filmlibrary
 object Constant {
     const val MOVIES_ADAPTER_COUNT_SPAN = 1
     const val MOVIES_ADAPTER_COUNT_SPAN2 = 3
-    const val COUNT_CATEGORY = 5
-    const val COUNT_MOVIES = 2000
     const val COUNT_MOVIES_BY_CATEGORY = 20
-    const val COUNT_MOVIES_BY_CATEGORIES = 10
+    const val COUNT_MOVIES_BY_GENRES = 10
+    const val COUNT_MOVIES_BY_TREND = 20
     const val READ_TIMEOUT: Int = 1000
     const val BASE_URL: String = "https://api.themoviedb.org/3/"
-    const val LANG: String = "&language=en-US"
+    const val LANG: String = "&language=ru-RU"
     const val URL_ITEM_MOVIE_1: String = "movie/"
     const val URL_API: String = "?api_key="
     const val URL_CATEGORIES_1: String = "genre/movie/list"
@@ -26,6 +25,17 @@ object Constant {
     const val DETAILS_RESPONSE_SUCCESS_EXTRA = "RESPONSE SUCCESS"
     const val BASE_IMAGE_URL: String = "https://image.tmdb.org/t/p/"
     const val IMAGE_POSTER_SIZE_1: String = "w185"
-    const val NAVIGATE_TO_MOVIEBYCAT: Int = R.id.action_navigation_home_to_moviesByCategoryFragment
-    const val NAVIGATE_TO_MOVIEINFO: Int = R.id.action_navigation_home_to_movieInfoFragment
+    const val NAVIGATE_FROM_GENRES_TO_MOVIES_BY_GENRE: Int = R.id.action_navigation_genres_to_moviesByGenresFragment2
+    const val NAVIGATE_FROM_TRENDS_TO_MOVIE_INFO: Int = R.id.action_navigation_trends_to_movieInfoFragment
+    const val NAVIGATE_FROM_GENRES_TO_MOVIE_INFO: Int = R.id.action_navigation_genres_to_movieInfoFragment2
+    const val NAVIGATE_FROM_MOVIES_BY_GENRES_TO_MOVIE_INFO: Int = R.id.action_moviesByGenresFragment_to_movieInfoFragment
+    const val NAVIGATE_FROM_SEARCH_TO_MOVIE_INFO: Int = R.id.action_navigation_search_to_movieInfoFragment
+    const val URL_TREND: String = "movie/"
+    const val URL_LATEST: String = "latest"
+    const val URL_NOW_PLAYING: String = "now_playing"
+    const val URL_POPULAR: String = "popular"
+    const val URL_TOP_RATED: String = "top_rated"
+    const val URL_UPCOMING: String = "upcoming"
+    const val URL_SEARCH_1: String = "search/movie"
+    const val URL_SEARCH_2: String = "&query="
 }
