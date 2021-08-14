f75e55a83aeb1566965a3273d8ff04410e6edc39
boris <borisov21011986@rambler.ru>
2021-08-14 23:13:09 +0400

    Создал классы жанр. Сохраняю соджержимое фрагментов

---
+++ app/src/main/java/com/android/filmlibrary/view/genres/GenresFragment.kt
@@ -0,0 +1,190 @@
+package com.android.filmlibrary.view.genres
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.filmlibrary.Constant
+import com.android.filmlibrary.GlobalVariables
+import com.android.filmlibrary.R
+import com.android.filmlibrary.databinding.GenresFragmentBinding
+import com.android.filmlibrary.model.AppState
+import com.android.filmlibrary.model.data.Genre
+import com.android.filmlibrary.model.data.MoviesByGenre
+import com.android.filmlibrary.view.showSnackBar
+import com.android.filmlibrary.viewmodel.genres.GenresViewModel
+
+class GenresFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = GenresFragment()
+    }
+
+    private lateinit var recyclerView: RecyclerView
+    private var moviesByGenres: List<MoviesByGenre> = ArrayList()
+    private var genres: List<Genre> = ArrayList()
+    private val adapter = GenresFragmentAdapter()
+    private val viewModel: GenresViewModel by lazy {
+        ViewModelProvider(this).get(GenresViewModel::class.java)
+    }
+
+    private var _binding: GenresFragmentBinding? = null
+    private val binding
+        get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = GenresFragmentBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun renderGenres(data: AppState) {
+        when (data) {
+            is AppState.SuccessGenres -> {
+                Log.v(
+                    "Debug1",
+                    "CategoriesFragment renderData AppState.SuccessCategories data.catgoriesData.size=" + data.genreData.size
+                )
+                val genresData = data.genreData
+                binding.loadingLayoutCat.visibility = View.GONE
+                fillData(genresData)
+            }
+            is AppState.Loading -> {
+                binding.loadingLayoutCat.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                binding.loadingLayoutCat.visibility = View.GONE
+                data.error.message?.let {
+                    binding.loadingLayoutCat.showSnackBar(it, R.string.ReloadMsg) {
+                        viewModel.getGenresFromRemoteSource()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun renderMoviesByGenres(data: AppState) {
+        when (data) {
+            is AppState.SuccessMoviesByGenres -> {
+                Log.v(
+                    "Debug1",
+                    "CategoriesFragment renderData AppState.SuccessMoviesByCategory data.moviesByCategory.size=" + data.moviesByGenres.size
+                )
+                moviesByGenres = data.moviesByGenres
+                binding.loadingLayoutCat.visibility = View.GONE
+                //fillMoviesByGenres(moviesByGenres)
+                adapter.fillMoviesByGenres(moviesByGenres)
+            }
+            is AppState.Loading -> {
+                binding.loadingLayoutCat.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                binding.loadingLayoutCat.visibility = View.GONE
+                data.error.message
+                data.error.message?.let {
+                    binding.loadingLayoutCat.showSnackBar(it, R.string.ReloadMsg) {
+                        viewModel.getGenresFromRemoteSource()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun fillData(genreData: List<Genre>) {
+        Log.v("Debug1", "CategoriesFragment fillData")
+        genres = genreData
+
+        viewModel.getMoviesByGenresFromRemoteSource(genres)
+
+        val observer = Observer<AppState> { appState ->
+            renderMoviesByGenres(appState)
+        }
+        viewModel.getData2().observe(viewLifecycleOwner, observer)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.v("Debug1", "CategoriesFragment onViewCreated")
+
+        recyclerView = binding.rvCat
+        recyclerView.layoutManager = GridLayoutManager(context, Constant.MOVIES_ADAPTER_COUNT_SPAN)
+        recyclerView.adapter = adapter
+
+        if ((requireActivity().application as GlobalVariables).genres.isNotEmpty())
+            genres = (requireActivity().application as GlobalVariables).genres
+
+        if ((requireActivity().application as GlobalVariables).moviesByGenres.isNotEmpty())
+            moviesByGenres = (requireActivity().application as GlobalVariables).moviesByGenres
+
+
+        adapter.setOnGenresClickListener { categoryId ->
+            activity?.supportFragmentManager?.let {
+                Log.v(
+                    "Debug1",
+                    "CategoriesFragment onViewCreated setOnCategoryClickListener categoryId=$categoryId"
+                )
+                var genre = genres.first()
+                for (genreItem in genres)
+                    if (genreItem.id == categoryId)
+                        genre = genreItem
+                val navHostFragment: NavHostFragment =
+                    activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+                navHostFragment.navController.navigate(
+                    Constant.NAVIGATE_FROM_GENRES_TO_MOVIES_BY_GENRE, //Вынес в константы
+                    Bundle().apply {
+                        putParcelable("category", genre)
+                    }
+                )
+            }
+        }
+
+        adapter.setOnMovieClickListener { movieId ->
+            Log.v("Debug1",
+                "CategoriesFragment onViewCreated setOnMovieClickListener movieId=$movieId")
+            val navHostFragment: NavHostFragment =
+                activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            navHostFragment.navController.navigate(
+                Constant.NAVIGATE_FROM_GENRES_TO_MOVIE_INFO,  //Вынес в константы
+                Bundle().apply {
+                    putInt("movieId", movieId)
+                }
+            )
+        }
+
+
+        if (moviesByGenres.isNotEmpty()) {
+            Log.v("Debug1",
+                "MoviesByGenreFragment onViewCreated moviesBySearch.isNotEmpty() moviesBySearch=")
+            adapter.fillMoviesByGenres(moviesByGenres)
+        } else {
+            val observer = Observer<AppState> { appState ->
+                renderGenres(appState)
+            }
+            viewModel.getData().observe(viewLifecycleOwner, observer)
+            viewModel.getGenresFromRemoteSource()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (requireActivity().application as GlobalVariables).genres = genres
+        (requireActivity().application as GlobalVariables).moviesByGenres = moviesByGenres
+        Log.v("Debug1", "MoviesByGenreFragment onStop")
+    }
+
+}
---
+++ app/src/main/java/com/android/filmlibrary/view/genres/GenresFragmentAdapter.kt
@@ -0,0 +1,149 @@
+package com.android.filmlibrary.view.genres
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.android.filmlibrary.Constant
+import com.android.filmlibrary.R
+import com.android.filmlibrary.databinding.ItemGenreBinding
+import com.android.filmlibrary.model.data.Genre
+import com.android.filmlibrary.model.data.Movie
+import com.android.filmlibrary.model.data.MoviesByGenre
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.collections.ArrayList
+
+class GenresFragmentAdapter : RecyclerView.Adapter<GenresFragmentAdapter.MyViewHolder>() {
+
+    //-------------------------------------------------------------------
+    private var onMovieClickListener: (Int) -> Unit = {}
+
+    fun setOnMovieClickListener(onMovieClickListener: (Int) -> Unit) {
+        this.onMovieClickListener = onMovieClickListener
+    }
+
+    //-------------------------------------------------------------------
+    private var onCategoryClickListener: (Int) -> Unit = {}
+
+    fun setOnGenresClickListener(onCategoryClickListener: (Int) -> Unit) {
+        this.onCategoryClickListener = onCategoryClickListener
+    }
+
+    //-------------------------------------------------------------------
+
+    private var moviesByCategory: List<MoviesByGenre> = ArrayList()
+
+    fun fillMoviesByGenres(moviesByCategory: List<MoviesByGenre>) {
+        Log.v(
+            "Debug1",
+            "CategoriesAdapter fillMoviesByCategory moviesByCategory.size=" + moviesByCategory.size
+        )
+        this.moviesByCategory = moviesByCategory
+        notifyDataSetChanged()
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        Log.v("Debug1", "CategoriesAdapter onCreateViewHolder")
+
+        val binding = ItemGenreBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return MyViewHolder(binding, parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        Log.v("Debug1", "CategoriesAdapter onBindViewHolder")
+        holder.categoryName.text = moviesByCategory[position].genre.title
+        moviesByCategory[position].genre.id.let {
+            holder.categoryId = moviesByCategory[position].genre.id
+        }
+        if (position != -1) {
+            holder.setData(moviesByCategory[position])
+            holder.categoryId = moviesByCategory[position].genre.id
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return moviesByCategory.size
+    }
+
+    inner class MyViewHolder(private val binding: ItemGenreBinding, parent: ViewGroup) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        private val parentLoc: ViewGroup = parent
+        val categoryName: TextView = itemView.findViewById(R.id.categoryName)
+        var categoryId: Int = 0
+        lateinit var genre: Genre
+        var movies: List<Movie> = ArrayList()
+
+
+        init {
+            Log.v("Debug1", "CategoriesAdapter MyViewHolder init categoryId=$categoryId")
+            binding.root.setOnClickListener {
+                onCategoryClickListener(categoryId)
+            }
+        }
+
+        fun setData(moviesByCategory: MoviesByGenre) {
+            Log.v("Debug1", "CategoriesAdapter MyViewHolder setData")
+            val linearLayoutItemCategory: LinearLayout = binding.linearLayoutItemCategory
+            val linearLayoutIntoScrollView: LinearLayout = binding.linearLayoutIntoScrollView
+
+            linearLayoutIntoScrollView.removeAllViews()
+            for (movie in moviesByCategory.movies) {
+                val viewItemMovie: View = LayoutInflater.from(parentLoc.context)
+                    .inflate(R.layout.item_movie, linearLayoutItemCategory, false)
+
+                val titleMovie = viewItemMovie.findViewById<TextView>(R.id.movieTitle)
+                val yearMovie = viewItemMovie.findViewById<TextView>(R.id.movieYear)
+                val catMovie = viewItemMovie.findViewById<TextView>(R.id.movieCat)
+                val ratedMovie = viewItemMovie.findViewById<TextView>(R.id.rated)
+                val posterMovie = viewItemMovie.findViewById<ImageView>(R.id.moviePoster)
+
+                Log.v("Debug1", "CategoriesAdapter MyViewHolder setData for movie.id" + movie.id)
+
+                posterMovie.setOnClickListener {
+                    onMovieClickListener(movie.id)
+                }
+
+                movie.posterUrl.let {
+                    if (movie.posterUrl != "" && movie.posterUrl != "-") {
+                        Glide.with(viewItemMovie.context)
+                            .load(Constant.BASE_IMAGE_URL + Constant.IMAGE_POSTER_SIZE_1 + movie.posterUrl)
+                            .into(posterMovie)
+                    } else {
+                        posterMovie.setImageResource(R.drawable.empty_poster)
+                    }
+                }
+
+                ratedMovie.text = movie.voteAverage.toString()
+                titleMovie.text = movie.title
+                if (movie.dateRelease != "") {
+                    val localDate = LocalDate.parse(movie.dateRelease,
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                    val formatter = DateTimeFormatter.ofPattern("yyyy")
+                    val formattedDate = localDate.format(formatter)
+                    yearMovie.text = formattedDate
+                } else {
+                    yearMovie.text = ""
+                }
+
+                if (movie.genres.isNotEmpty()){
+                    catMovie.text = movie.genres.first().title
+                }
+
+                linearLayoutIntoScrollView.addView(viewItemMovie)
+            }
+        }
+    }
+}
